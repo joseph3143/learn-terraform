fd372b27db1213b8c226f9042739a6bb74c942ba
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-03-08 11:59:36 +0530

    Updating learn-terraform

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -6,14 +6,18 @@ output  "sample" {
   value = var.sample
 }
 
+#getting value from another file
 variable "sample1" {}
 
 output "sample1" {
   value = var.sample1
 }
 
+#giving value in terminal
 variable "cli" {}
 
 output "cli" {
   value = var.cli
 }
+
+variable "input" {}
