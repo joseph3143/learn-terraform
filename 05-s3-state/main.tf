4bce8ed36963b7ecdea3e9968834f30c732b5d41
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-03-09 15:45:04 +0530

    Updating learn-terraform

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -11,6 +11,6 @@ resource "aws_instance" "ec2" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-064d0313949c3719d"]
   tags = {
-    Name = "test"
+    Name = "demo"
   }
 }
