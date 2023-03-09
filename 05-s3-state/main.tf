b7b3bf63219114f6feb46d7009e2330c52a72c19
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-03-09 11:51:47 +0530

    Updating learn-terraform

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-d1"
+    key = "05-s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
