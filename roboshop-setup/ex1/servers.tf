44aa3436769570f0a23b88d6ec431ed1408e661c
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-03-09 08:05:12 +0530

    Updating learn-terraform

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "frontend-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -14,6 +22,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "cart-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -25,6 +41,14 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "catalogue-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -32,6 +56,14 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "user-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "shipping" {
@@ -43,6 +75,13 @@ resource "aws_instance" "shipping" {
   }
 }
 
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "shipping-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 resource "aws_instance" "payment" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -50,6 +89,14 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "payment-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
 
 resource "aws_instance" "mongodb" {
@@ -61,6 +108,14 @@ resource "aws_instance" "mongodb" {
   }
 }
 
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "mongodb-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "mysql" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -70,6 +125,13 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "mysql-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
 resource "aws_instance" "redis" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -79,6 +141,13 @@ resource "aws_instance" "redis" {
   }
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "redis-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
 resource "aws_instance" "rabbitmq" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -86,4 +155,12 @@ resource "aws_instance" "rabbitmq" {
   tags = {
     Name = "rabbitmq"
   }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "rabbitmq-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
 }
