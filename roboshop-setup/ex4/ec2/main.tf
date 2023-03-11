faa8bde2e4b7a306ffd129f4fe818e5ac9937bcb
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-03-11 10:00:33 +0530

    Updating learn-terraform

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -18,7 +18,7 @@
    name    = "${var.component}-dev.devopsculture.online"
    type    = "A"
    ttl     = 30
-   records = [aws_instance.frontend.private_ip]
+   records = [aws_instance.ec2.private_ip]
  }
 
  variable "component" {}
