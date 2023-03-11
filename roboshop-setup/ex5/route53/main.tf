9f22951eaefead1265501297cfe0b6f069e173bf
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-03-11 11:48:08 +0530

    Updating learn-terraform

---
+++ roboshop-setup/ex5/route53/main.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "record" {
+  zone_id = "Z0628828KZGH4QGSXZXN"
+  name    = "${var.component}-dev.devopsculture.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
